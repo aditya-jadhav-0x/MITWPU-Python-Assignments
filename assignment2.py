4dc2360c613b1ebffda6fde22f5355db89d01c00
Aditya Jadhav <adityarajeshjadhav2008@gmail.com>
2026-09-06 23:21:22 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -13,3 +13,27 @@ else:
 
 # Printing the final output
 print("The largest number is:", largest)
+
+#-----------------------------
+# Accept Marks in Math from user
+math_marks = int(input("Enter your Mathematics marks: "))
+
+# Multi-condition chain to find the grade
+if math_marks >= 90 and math_marks <= 100:
+    grade = "O"
+elif math_marks >= 80 and math_marks <= 89:
+    grade = "A+"
+elif math_marks >= 70 and math_marks <= 79:
+    grade = "A"
+elif math_marks >= 60 and math_marks <= 69:
+    grade = "B"
+elif math_marks >= 50 and math_marks <= 59:
+    grade = "C"
+elif math_marks >= 40 and math_marks <= 49:
+    grade = "P"
+else:
+    grade = "F"
+
+# Display calculated grade output
+print("Your assigned Grade is:")
+print(grade)
